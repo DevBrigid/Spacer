11fa710d664d1c3a9a8c6b157b7a62fd3f8703f4
DevBrigid <mukamibrigid20@gmail.com>
2026-08-20 13:25:09 +0300

    add paymentsSlice simulating real STK Push pending/success flow

--- client/spacer/src/store/store.js
+++ client/spacer/src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import spacesReducer from './spacesSlice';
 import bookingsReducer from './bookingsSlice';
+import paymentsReducer from './paymentsSlice'
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         spaces: spacesReducer,
         bookings: bookingsReducer,
+        payments: paymentsReducer,
     },
 });
