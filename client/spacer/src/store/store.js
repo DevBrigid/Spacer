18bd30c88a23e7eedf053d487408f653e0384509
DevBrigid <mukamibrigid20@gmail.com>
2026-08-20 11:05:55 +0300

    add spacesSlice with fetchSpaces/fetchSpacesById thunks

--- client/spacer/src/store/store.js
+++ client/spacer/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
+import spacesReducer from './spacesSlice'
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        spaces: spacesReducer,
     },
 });
