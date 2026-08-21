47292edc7d33c7efeb3de9f642b31faa2223daa5
Chris Ndirangu <mathengechris@gmail.com>
2026-08-21 00:34:57 -0700

    Resolve App router merge conflict

--- client/spacer/src/App.jsx
+++ client/spacer/src/App.jsx
@@ -1,22 +1,12 @@
-<<<<<<< Updated upstream
-import './index.css'
-=======
 import './index.css';
->>>>>>> Stashed changes
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { loadUserFromStorage } from './store/authSlice';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-<<<<<<< Updated upstream
-import PrivateRoute from './routes/PrivateRoute';
-=======
-
-import { loadUserFromStorage, fetchCurrentUser } from './store/authSlice';
-
->>>>>>> Stashed changes
 import AdminRoute from './routes/AdminRoute';
+import ClientLayout from './layouts/ClientLayout';
+import AdminLayout from './layouts/AdminLayout';
 
 import AuthPage from './pages/public/AuthPage';
 import LandingPage from './pages/public/LandingPage';
@@ -36,26 +26,15 @@ import ManageUsers from './pages/admin/ManageUsers';
 import ManageSpaces from './pages/admin/ManageSpaces';
 import AdminProfile from './pages/admin/AdminProfile';
 
-import './App.css'
-import ClientLayout from './layouts/ClientLayout';
-import AdminLayout from './layouts/AdminLayout';
-
 function App() {
   const dispatch = useDispatch();
-  const { token, authChecked, fetchCurrentUser } = useSelector((state) => state.auth);
+  const { token, authChecked } = useSelector((state) => state.auth);
 
-  //restore session on app load
   useEffect(() => {
     dispatch(loadUserFromStorage());
   }, [dispatch]);
 
-  useEffect(() => {
-    if (token) {
-      dispatch(fetchCurrentUser());
-    }
-  }, [token, dispatch, fetchCurrentUser]);
-
-  if (!authChecked && localStorage.getItem('token')) {
+  if (!authChecked && token) {
     return <p>Loading...</p>;
   }
 
@@ -63,32 +42,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<ClientLayout />}>
-<<<<<<< Updated upstream
-          <Route path='/' element={<LandingPage/>} />
-          <Route path='/spaces' element={<BrowsePage/>} />
-          <Route path='/spaces/:id' element={<SpaceDetails/>} />
-          <Route path='/login' element={<LoginPage/>} />
-          <Route path='/register' element={<RegisterPage/>} />
-          <Route path='/forgot-password' element={<ForgotPasswordPage/>} />
-        
-
-          {/* Client - login required */}
-          <Route path='/spacer' element={<PrivateRoute><ClientDashboard/></PrivateRoute>}/>
-          <Route path='/spacer/profile' element={<PrivateRoute><ClientProfile/></PrivateRoute>}/>
-          <Route path='/spacer/bookings' element={<PrivateRoute><MyBookings/></PrivateRoute>}/>
-          <Route path='/spacer/booking/:spacerId' element={<PrivateRoute><BookingPage/></PrivateRoute>}/>
-          <Route path='/spacer/agreement' element={<PrivateRoute><AgreementPage/></PrivateRoute>}/>
-          <Route path='/spacer/payment' element={<PrivateRoute><PaymentPage/></PrivateRoute>}/>
-        </Route>
-
-        {/* Admin - admin role required */}
-        <Route path='/admin' element={<AdminRoute><AdminLayout/></AdminRoute>}>
-          <Route index element={<AdminDashBoard/>}/>
-          <Route path='/admin/spaces' element={<AdminRoute><ManageSpaces/></AdminRoute>}/>
-          <Route path='/admin/users' element={<AdminRoute><ManageUsers/></AdminRoute>}/>
-          <Route path='/admin/bookings' element={<AdminRoute><BookingHistory/></AdminRoute>}/>
-          <Route path='admin/profile' element={<AdminRoute><AdminProfile/></AdminRoute>}/>
-=======
           <Route path="/" element={<LandingPage />} />
           <Route path="/spaces" element={<BrowseSpaces />} />
           <Route path="/spaces/:id" element={<SpaceDetails />} />
@@ -109,15 +62,10 @@ function App() {
           <Route path="users" element={<ManageUsers />} />
           <Route path="bookings" element={<BookingHistory />} />
           <Route path="profile" element={<AdminProfile />} />
->>>>>>> Stashed changes
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-<<<<<<< Updated upstream
-export default App
-=======
 export default App;
->>>>>>> Stashed changes
